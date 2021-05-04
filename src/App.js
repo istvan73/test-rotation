00ec30ac51cc6f87a21399d4118a4b67cff171a3
aistvan73 <istvan.agoston@accenture.com>
2021-05-04 10:23:54 +0300

    trying smooth approach

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-    const handleTest1 = () => {
+    useEffect(() => {
         window.screen.orientation.lock('landscape').then(() => console.log('successfully locked orientation'), () => console.log('error occurred'));
-    }
 
+    }, []);
 
 
     return (
         <div className="App">
             <h2>Hi, this is a page as empty, as the toilet paper section during covid! </h2>
             <p>Just so it has some meaning, here is a cool music: </p>
-            <a href='https://www.youtube.com/watch?v=FK-YmV1eVaU'>Cool music</a>
+            <a href='https://www.youtube.com/watch?v=h5xU2bghopw'>Cool music</a>
             <p>Only solution 1 works, and we have a default 'solution', which may or may not work on safari</p>
             <ul>
                 <li><button onClick={handleTest1}>Test1</button></li>
