f29c1bddfd19a56acfdadbaa2b14326033b35fc0
aistvan73 <istvan.agoston@accenture.com>
2021-05-04 12:01:04 +0300

    trying a safari proof solution

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
 
-    useEffect(() => {
-        window.screen.orientation.lock('landscape').then(() => console.log('successfully locked orientation'), () => console.log('error occurred'));
+    // useEffect(() => {
+    //     window.screen.orientation.lock('landscape').then(() => console.log('successfully locked orientation'), () => console.log('error occurred'));
 
-    }, []);
+    // }, []);
 
 
     return (
@@ -14,7 +14,6 @@ function App() {
             <h2>Hi, this is a page as empty, as the toilet paper section during covid! </h2>
             <p>Just so it has some meaning, here is a cool music: </p>
             <a href='https://www.youtube.com/watch?v=h5xU2bghopw'>Cool music</a>
-            <p>Only solution 1 works, and we have a default 'solution', which may or may not work on safari</p>
             <ul>
                 <li><button>Test1</button></li>
                 <li><button>Test2</button></li>
